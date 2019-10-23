8e6ac1026bca4be910c4f74421835f01ef7e31ce
Andrew Pratt <apratt957@gmail.com>
2019-10-23 16:00:00 -0500

    recursion almost working

--- mergesort.js
+++ mergesort.js
@@ -1,37 +1,54 @@
 function split(wholeArray) {
-    const firstHalf = []
-    const secondHalf = []
-    let count = 0;
+  const firstHalf = [];
+  const secondHalf = [];
+  let count = 0;
 
-    for (let i = 0; i < Math.floor(wholeArray.length / 2); i++) {
-        firstHalf[i] = wholeArray[i];
-    }
-    for (let i = Math.floor(wholeArray.length / 2); i < wholeArray.length; i++ ) {
-            secondHalf[count] = wholeArray[i]
-            count++;
-        }
+  for (let i = 0; i < Math.floor(wholeArray.length / 2); i++) {
+    firstHalf[i] = wholeArray[i];
+  }
+  for (let i = Math.floor(wholeArray.length / 2); i < wholeArray.length; i++) {
+    secondHalf[count] = wholeArray[i];
+    count++;
+  }
 
-    return [firstHalf, secondHalf]
+  return [firstHalf, secondHalf];
 }
 
 function merge(firstHalf, secondHalf) {
-    let firstCount = 0;
-    let secondCount = 0;
-    const mergedArr = []
+  let firstCount = 0;
+  let secondCount = 0;
+  const mergedArr = [];
 
-    let idx = 0;
+  let idx = 0;
 
-    while ((firstCount + secondCount) !== (firstHalf.length + secondHalf.length)){
-        if (firstHalf[firstCount] < secondHalf[secondCount]){
-            mergedArr[idx] = firstHalf[firstCount]
-            idx++;
-            firstCount++;
-        } else {
-            mergedArr[idx] = secondHalf[secondCount]
-            idx++;
-            secondCount++;
-        }
+  while (firstCount + secondCount !== firstHalf.length + secondHalf.length) {
+    if (firstHalf[firstCount] < secondHalf[secondCount]) {
+      mergedArr[idx] = firstHalf[firstCount];
+      idx++;
+      firstCount++;
+    } else {
+      mergedArr[idx] = secondHalf[secondCount];
+      idx++;
+      secondCount++;
     }
+  }
 
-    return mergedArr;
+  return mergedArr;
 }
+
+function mergeSort(array) {
+  // debugger;
+  if (array.length <= 1) {
+    return array;
+  } else {
+    let [arr1, arr2] = split(array);
+    let splitArray1 = mergeSort(arr1);
+    let splitArray2 = mergeSort(arr2);
+
+    return merge(splitArray1, splitArray2);
+  }
+}
+
+const test1 = [4, 12, 5, 13, 1, 6];
+const test2 = [7, 3, 2, 10, 9, 8];
+const test3 = [2, 4, 7, 1, 5, 12, 8];
