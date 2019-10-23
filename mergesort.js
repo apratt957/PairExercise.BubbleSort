06cb9dfd5131206084eac51f546fdc9704ae8965
Chris Pietka <chrispietka@yahoo.com>
2019-10-23 14:35:44 -0500

    added merge function, super optimized

--- mergesort.js
+++ mergesort.js
@@ -1,14 +1,37 @@
 function split(wholeArray) {
     const firstHalf = []
     const secondHalf = []
+    let count = 0;
 
-    for (let i = 0; i < wholeArray.length; i++) {
-        if (i < Math.floor(wholeArray.length / 2)) {
-            firstHalf.push(wholeArray[i])
+    for (let i = 0; i < Math.floor(wholeArray.length / 2); i++) {
+        firstHalf[i] = wholeArray[i];
+    }
+    for (let i = Math.floor(wholeArray.length / 2); i < wholeArray.length; i++ ) {
+            secondHalf[count] = wholeArray[i]
+            count++;
+        }
+
+    return [firstHalf, secondHalf]
+}
+
+function merge(firstHalf, secondHalf) {
+    let firstCount = 0;
+    let secondCount = 0;
+    const mergedArr = []
+
+    let idx = 0;
+
+    while ((firstCount + secondCount) !== (firstHalf.length + secondHalf.length)){
+        if (firstHalf[firstCount] < secondHalf[secondCount]){
+            mergedArr[idx] = firstHalf[firstCount]
+            idx++;
+            firstCount++;
         } else {
-            secondHalf.push(wholeArray[i])
+            mergedArr[idx] = secondHalf[secondCount]
+            idx++;
+            secondCount++;
         }
     }
 
-    return [firstHalf, secondHalf]
+    return mergedArr;
 }
